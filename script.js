80036df2a33573b6d5da91f96f8565714d9b7703
bhavnachihla <141863406+bhavnachihla@users.noreply.github.com>
2024-02-17 22:33:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+ function generateURL() {
+      var nameValue = encodeURIComponent(document.getElementById('name').value);
+      var yearValue = encodeURIComponent(document.getElementById('year').value);
+
+      var queryString = `?name=${nameValue}&year=${yearValue}`;
+      document.getElementById('url').innerText = `https://localhost:8080/${queryString}`;
+    }
